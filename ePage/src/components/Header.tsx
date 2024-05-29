86cfeecb83284ce9a08f064452da12192e24c6e1
PabloSan1997 <jos_pab_997@hotmali.com>
2024-05-29 13:25:33 -0600

    Se agregaron estilos al header

--- ePage/src/components/Header.tsx
+++ ePage/src/components/Header.tsx
@@ -2,7 +2,7 @@ import { Link, NavLink } from "react-router-dom";
 import { useAppDispatch, useAppSelector } from "../hooks";
 import { commersActions } from "../splice/commerSlice";
 import { stringRoutes } from "../utilities/routes";
-
+import '../styles/header.scss';
 
 export default function Header() {
     const dispatch = useAppDispatch();
@@ -10,24 +10,26 @@ export default function Header() {
     return (
         <header>
             <h1>Tienda</h1>
-            {
-                state.token ? (
-                    <>
-                        <nav>
-                            <Link to={stringRoutes.home}>Home</Link>
-                            <Link to={stringRoutes.carrito}>{state.userInfo.name}</Link>
+            <div className="button_part">
+                {
+                    state.token ? (
+                        <>
+                            <nav>
+                                <Link to={stringRoutes.home}>Home</Link>
+                                <Link to={stringRoutes.carrito}>{state.userInfo.name}</Link>
+                            </nav>
+                            <button
+                                onClick={() => dispatch(commersActions.logout())}
+                            >Logout</button>
+                        </>
+                    ) : (
+                        <nav className="logins">
+                            <NavLink className={({isActive})=>isActive?'desactive':'sec'} to={stringRoutes.login}>Login</NavLink>
+                            <NavLink className={({isActive})=>isActive?'desactive':'sec'} to={stringRoutes.register}>Register</NavLink>
                         </nav>
-                        <button
-                            onClick={() => dispatch(commersActions.logout())}
-                        >Logout</button>
-                    </>
-                ) : (
-                    <nav>
-                        <NavLink to={stringRoutes.login}>Login</NavLink>
-                        <NavLink to={stringRoutes.register}>Register</NavLink>
-                    </nav>
-                )
-            }
+                    )
+                }
+            </div>
         </header>
     );
 }
--- ePage/src/index.tsx
+++ ePage/src/index.tsx
@@ -2,7 +2,7 @@ import ReactDOM from "react-dom/client";
 import { App } from "./App";
 import { Provider } from "react-redux";
 import { store } from "./store";
-
+import './styles/index.scss';
 
 const root = ReactDOM.createRoot(document.querySelector('#root')!);
 
@@ -10,5 +10,4 @@ root.render(
     <Provider store={store}>
         <App />
     </Provider>
-
 );
