f3664ccf3d54275e2ab5e19713870f490487f85b
PabloSan1997 <jos_pab_997@hotmali.com>
2024-06-19 11:48:11 -0600

    Se corigio problemas con eslint

--- ePage/src/layouts/AddCategory.tsx
+++ ePage/src/layouts/AddCategory.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { FormEvent, useEffect } from "react";
 import { useAppDispatch, useAppSelector } from "../hooks";
 import { addCategoryExtraReducer } from "../splice/extraReducer/productExtraReducer";
